20d9a6679d742a0b1c10c5cca2d1adc5e6fe6ca4
Giuseppe <55535562+gpalumbo1@users.noreply.github.com>
2025-07-25 15:02:34 +0200

    Delete lib directory

--- lib/lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-class UserModel {
-  final String uid;
-  final String nickname;
-  UserModel({required this.uid, required this.nickname});
-  factory UserModel.fromMap(String id, Map<String, dynamic> data) => UserModel(
-    uid: id,
-    nickname: data['nickname'] ?? '',
-  );
-}
