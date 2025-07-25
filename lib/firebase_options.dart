fab60fbbfa87ed41725290635ec8ab63a83b2e39
Giuseppe <55535562+gpalumbo1@users.noreply.github.com>
2025-07-25 15:02:57 +0200

    Add files via upload

---
+++ lib/firebase_options.dart
@@ -0,0 +1,20 @@
+// TODO: Replace with actual Firebase configuration.
+// You can generate this file via FlutterFire CLI:
+// flutterfire configure --project splitdo-app
+
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: 'AIzaSyA3plncQb0Mwd_CPAMGt1vWqlZ2nJPBcPo',
+    appId: '1:358529967253:android:6da951f65b25da056f679c',
+    messagingSenderId: '358529967253',
+    projectId: 'splitdo-app',
+
+    // Se serve, aggiungi qui anche:
+    // authDomain: '...',
+    // storageBucket: '...',
+    // measurementId: '...',
+  );
+}
+
