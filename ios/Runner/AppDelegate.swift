b561c7d9c76e1e642a141016a9395b4f9315f3eb
Giuseppe <55535562+gpalumbo1@users.noreply.github.com>
2025-08-13 10:06:04 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,56 +10,26 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Inizializza Firebase
     FirebaseApp.configure()
 
+    // Richiesta permesso notifiche (solo iOS 10+)
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
-
       let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { granted, error in
-          // Puoi gestire error o granted qui
-        }
-      )
+      UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
     } else {
+      // Supporto vecchi iOS (quasi sempre inutile oggi)
       let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
       application.registerUserNotificationSettings(settings)
     }
 
+    // Registrazione per le notifiche push
     application.registerForRemoteNotifications()
 
+    // Registra i plugin Flutter
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // Chiamato quando APNs assegna il token univoco del dispositivo
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-  // Mostra notifiche in foreground (iOS 10+)
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    willPresent notification: UNNotification,
-    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-  ) {
-    completionHandler([.alert, .badge, .sound])
-  }
-
-  // Gestisce il tap su una notifica
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    didReceive response: UNNotificationResponse,
-    withCompletionHandler completionHandler: @escaping () -> Void
-  ) {
-    completionHandler()
   }
 }
