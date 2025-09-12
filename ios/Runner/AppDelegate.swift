e0796d9726841a9e3971dd8550498bb14a6efe37
Giuseppe <55535562+gpalumbo1@users.noreply.github.com>
2025-09-12 17:22:23 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -17,7 +17,7 @@ import GoogleMobileAds
     FirebaseApp.configure()
 
     // Inizializza Google Mobile Ads
-    MobileAds.sharedInstance().start()
+    //MobileAds.sharedInstance().start()
 
     // Configura notifiche push
     configurePushNotifications(application)
